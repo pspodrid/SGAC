d4a48889ec29bd56968ad2e36caf34014dffb8ea
Student <example@example.com>
2019-11-15 09:33:14 -0800

    run test to take in two inputs, age and expected age

--- __tests__/tests.back.js
+++ __tests__/tests.back.js
@@ -5,7 +5,8 @@ describe('Sgac', () => {
 
 
   test('should take in a number and return the number', () => {
-    let sgac = new Sgac(5);
+    let sgac = new Sgac(5, 10);
     expect(sgac.age).toEqual(5);
+    expect(sgac.expected).toEqual(10);
   });
 });
--- src/back.js
+++ src/back.js
@@ -1,9 +1,18 @@
 export class Sgac {
-  constructor(age) {
+  constructor(age, expected) {
     this.age = age;
-
+    this.expected = expected;
+    this.mercAge = 0;
+    this.mercAgeEx = 0;
+    this.venAge = 0;
+    this.venAgeEx = 0;
+    this.marsAge = 0;
+    this.marsAgeEx = 0;
+    this.jupAge = 0;
+    this.jupAgeEx = 0;
   }
 
   mercuryCalc() {
+    return "hi"
   }
 }
